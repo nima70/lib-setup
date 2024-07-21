6040f912c863b8a08f25ebfc372a9175a960abf8
nima70 <nima.shokofar@gmail.com>
2024-07-21 14:54:49 -0400

    I fixed the build issue

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-import "module-alias/register";
-import express, { Express, Request, Response } from "express";
-
-export interface testRequest {
-  id: string;
-}
-
-const app: Express = express();
-const port = 3000;
-
-app.get("/", (req: Request<testRequest>, res: Response) => {
-  res.send(`Hello world: ${req.body.id}`);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+export const display = () => {
+  console.log("hellow world...!!!");
+};
